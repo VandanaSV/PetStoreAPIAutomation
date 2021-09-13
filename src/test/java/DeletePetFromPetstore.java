cd41de5ccec8b6dcbc7c6d87b180040bc8111025
vandana.sv <vandana.sv@allianz.com>
2021-09-13 12:08:42 +0530

    change name

--- src/test/java/DeletePetFromPetstore.java
+++ src/test/java/DeletePetFromPetstore.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 
 public class DeletePetFromPetstore {
     @Test
-    public void FirstTestDELETE() {
+    public void deletePetById() {
         int orderID=6;
         RestAssured.given().contentType("application/json").when()
                 .delete("https://petstore.swagger.io/v2/store/order/{orderID}",orderID).then()
